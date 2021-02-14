de3d20258fbeecc0dcf801572411cbe19cc95ea1
Tommy Miland <kontakt@tmiland.com>
2021-02-14 19:47:15 +0100

    Sort current version by highest number

--- check.sh
+++ check.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 github_dektop_repo=shiftkey/desktop
-github_desktop_CUR_VERSION="$(find ./debian/ -name "GitHubDesktop-linux-*.deb" | sed 's/.*-\([0-9\.][0-9\.]*\).*/\1/' | head -n 1)"
+github_desktop_CUR_VERSION="$(find ./debian/ -name "GitHubDesktop-linux-*.deb" | sed 's/.*-\([0-9\.][0-9\.]*\).*/\1/' | sort -rnk3 | head -n 1)"
 github_desktop_NEW_VERSION="$(curl -s https://api.github.com/repos/$github_dektop_repo/releases | grep '"tag_name":' | sed -n 's/[^0-9.]*\([0-9.]*\).*/\1/p' | head -n 1)"
 echo "Current Version: $github_desktop_CUR_VERSION => New Version: $github_desktop_NEW_VERSION"
 
