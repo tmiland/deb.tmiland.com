6fcb5248dd8bd59cbc24faa315dbd07a599a723f
Tommy Miland <kontakt@tmiland.com>
2021-02-28 17:58:58 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,12 +3,17 @@
 set -e
 set -v
 
+# Detect absolute and full path as well as filename of this script
+cd "$(dirname "$0")" || exit
+CURRDIR=$(pwd)
+cd - > /dev/null || exit
+
 export KEYNAME=7C44CFE8E3B971486A2E0A280A2998CB2E6D61E0
 
 (
     set -e
     set -v
-
+    cd "${CURRDIR}" || exit
     cd ./debian/
 
     # Packages & Packages.gz
