422ad4522b685fbb063e2389ce018af2581d586b
Tommy Miland <kontakt@tmiland.com>
2025-06-20 16:35:10 +0200

    Update Icecat version to 128.9.0

--- check.sh
+++ check.sh
@@ -7,58 +7,74 @@ cd "$(dirname "$0")" || exit
 CURRDIR=$(pwd)
 cd - > /dev/null || exit
 
-GitHubDesktop() {
-  github_dektop_repo=shiftkey/desktop
-  cd "${CURRDIR}" || exit
-  github_desktop_CUR_VERSION="$(find ./debian/ -name "GitHubDesktop-linux-*.deb" | sed 's/.*-\([0-9\.][0-9\.]*\).*/\1/' | sort -rnk3 | head -n 1)"
-  github_desktop_NEW_VERSION="$(curl -sSL https://api.github.com/repos/$github_dektop_repo/releases | grep '"tag_name":' | sed -n 's/[^0-9.]*\([0-9.]*\).*/\1/p' | head -n 1)"
-  echo "Current GitHub Desktop Version: $github_desktop_CUR_VERSION => New Version: $github_desktop_NEW_VERSION"
-
-  if [[ "$github_desktop_CUR_VERSION" < "$github_desktop_NEW_VERSION" ]]; then
-
-    echo "Downloading new github-desktop version $github_desktop_NEW_VERSION" | mail -s "Downloading new github-desktop version $github_desktop_NEW_VERSION" $email
-    cd "${CURRDIR}" || exit
-    curl -sSL https://api.github.com/repos/$github_dektop_repo/releases \
-      | grep "browser_download_url.*deb" \
-      | cut -d : -f 2,3 \
-      | tr -d \" \
-      | head -n 1 \
-      | wget -qi -
-
-    deb_file="$(find . -name "GitHubDesktop-linux*-$github_desktop_NEW_VERSION-linux*.deb" 2>/dev/null)"
-    mv "$deb_file" ./debian/
-    . ./update.sh
-    git add -A
-    git commit -m "Update github-desktop version to $github_desktop_NEW_VERSION"
-    git push -u origin master
-    git tag -a "$github_desktop_NEW_VERSION" -m "Update github-desktop version from $github_desktop_CUR_VERSION to $github_desktop_NEW_VERSION"
-    git push --tags origin master
-    exit
-
-  else
-    echo "Latest GitHub Desktop version already installed"
-  fi
-}
+# GitHubDesktop() {
+#   github_dektop_repo=shiftkey/desktop
+#   cd "${CURRDIR}" || exit
+#   github_desktop_CUR_VERSION="$(find ./debian/ -name "GitHubDesktop-linux-*.deb" | sed 's/.*-\([0-9\.][0-9\.]*\).*/\1/' | sort -rnk3 | head -n 1)"
+#   github_desktop_NEW_VERSION="$(curl -sSL https://api.github.com/repos/$github_dektop_repo/releases | grep '"tag_name":' | sed -n 's/[^0-9.]*\([0-9.]*\).*/\1/p' | head -n 1)"
+#   echo "Current GitHub Desktop Version: $github_desktop_CUR_VERSION => New Version: $github_desktop_NEW_VERSION"
+# 
+#   if [[ "$github_desktop_CUR_VERSION" < "$github_desktop_NEW_VERSION" ]]; then
+# 
+#     echo "Downloading new github-desktop version $github_desktop_NEW_VERSION" | mail -s "Downloading new github-desktop version $github_desktop_NEW_VERSION" $email
+#     cd "${CURRDIR}" || exit
+#     curl -sSL https://api.github.com/repos/$github_dektop_repo/releases \
+#       | grep "browser_download_url.*deb" \
+#       | cut -d : -f 2,3 \
+#       | tr -d \" \
+#       | head -n 1 \
+#       | wget -qi -
+# 
+#     deb_file="$(find . -name "GitHubDesktop-linux*-$github_desktop_NEW_VERSION-linux*.deb" 2>/dev/null)"
+#     mv "$deb_file" ./debian/
+#     . ./update.sh
+#     git add -A
+#     git commit -m "Update github-desktop version to $github_desktop_NEW_VERSION"
+#     git push -u origin master
+#     #git tag -a "$github_desktop_NEW_VERSION" -m "Update github-desktop version from $github_desktop_CUR_VERSION to $github_desktop_NEW_VERSION"
+#     git push --tags origin master
+#     exit
+# 
+#   else
+#     echo "Latest GitHub Desktop version already installed"
+#   fi
+# }
 
 gnuzilla() {
-  ICECATE_URL=https://download.opensuse.org/repositories/home:/losuler:/icecat/Debian_11/amd64
+  ICECAT_REPO_DIR="../GNU-IceCat"
+  # ICECAT_LATEST_VERSION=$(curl -s https://icecatbrowser.org/all_downloads.html |
+  #   grep -Po 'h2>\K.*(?=</h2)' |
+  #   head -n 1 |
+  # sed "s|Icecat Version:||g")
+
   cd "${CURRDIR}" || exit
-  ICECAT_CUR_VERSION="$(find ./debian/ -name "icecat_*.deb" | cut -d _ -f 2 | sort -rnk3 | head -n 1)"
-  ICECAT_NEW_VERSION="$(curl -sSL $ICECATE_URL | grep -oP 'href="icecat_\K[0-9]+\.[0-9]+\.[0-9](-[0-9])?' | sort -rnk3 | head -n 1)"
+  ICECAT_CUR_VERSION=$(echo $(grep -Poh "(?<=Version: )([0-9]|\.)*(?=\s|$)" "${ICECAT_REPO_DIR}"/amd64/DEBIAN/*))
+  ICECAT_NEW_VERSION=$(curl -s https://icecatbrowser.org/all_downloads.html |
+    grep -Po 'b>\K.*(?=</b)' |
+    head -n 1 |
+    sed -n 's/[^0-9.]*\([0-9.]*\).*/\1/p' |
+    # Stript trailing .
+  sed 's/.$//')
+
   echo "Current Icecat Version: $ICECAT_CUR_VERSION => New Version: $ICECAT_NEW_VERSION"
   
-  if [[ "$ICECAT_CUR_VERSION" < ${ICECAT_NEW_VERSION} ]]; then
+  #if [[ "$ICECAT_CUR_VERSION" < ${ICECAT_NEW_VERSION} ]]; then
+    if dpkg --compare-versions "$ICECAT_CUR_VERSION" lt "$ICECAT_NEW_VERSION"
+    then
+
     echo "Downloading new Icecat version $ICECAT_NEW_VERSION" | mail -s "Downloading new Icecat version $ICECAT_NEW_VERSION" $email
-    cd "${CURRDIR}" || exit
-    wget $ICECATE_URL/icecat_"${ICECAT_NEW_VERSION}"_amd64.deb
     
-    deb_file="$(find . -name "icecat_${ICECAT_NEW_VERSION}_amd64.deb" 2>/dev/null)"
+    cd "${ICECAT_REPO_DIR}" || exit
+    ./package.sh
+    cd - || exit 1
+    
+    deb_file="$(find "${ICECAT_REPO_DIR}" -name "icecat_${ICECAT_NEW_VERSION}_amd64.deb" 2>/dev/null)"
     mv "$deb_file" ./debian/
     . ./update.sh
     git add -A
     git commit -m "Update Icecat version to $ICECAT_NEW_VERSION"
     git push -u origin master
-    git tag -a "${ICECAT_NEW_VERSION}" -m "Update Icecat version from $ICECAT_CUR_VERSION to $ICECAT_NEW_VERSION"
+    #git tag -a "${ICECAT_NEW_VERSION}" -m "Update Icecat version from $ICECAT_CUR_VERSION to $ICECAT_NEW_VERSION"
     git push --tags origin master
     exit
   else
@@ -91,7 +107,7 @@ snapserver() {
     git add -A
     git commit -m "Update snapserver version to $snapserver_NEW_VERSION"
     git push -u origin master
-    git tag -a "$snapserver_NEW_VERSION" -m "Update snapserver version from $snapserver_CUR_VERSION to $snapserver_NEW_VERSION"
+    #git tag -a "$snapserver_NEW_VERSION" -m "Update snapserver version from $snapserver_CUR_VERSION to $snapserver_NEW_VERSION"
     git push --tags origin master
     exit
 
@@ -124,7 +140,7 @@ snapclient() {
     git add -A
     git commit -m "Update snapclient version to $snapclient_NEW_VERSION"
     git push -u origin master
-    git tag -a "$snapclient_NEW_VERSION" -m "Update snapclient version from $snapclient_CUR_VERSION to $snapclient_NEW_VERSION"
+    #git tag -a "$snapclient_NEW_VERSION" -m "Update snapclient version from $snapclient_CUR_VERSION to $snapclient_NEW_VERSION"
     git push --tags origin master
     exit
 
